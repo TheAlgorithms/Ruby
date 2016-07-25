04d7fc36964f30f0bf2a191627ee54e5b654e95e
Chetan Kaushik <dynamitechetan@gmail.com>
2016-07-25 23:03:27 +0530

    Added Bubble Sort

---
+++ BubbleSort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+puts "Enter a list of numbers seprated by space"
+
+list = gets 
+bubble_sort(list)
+print list

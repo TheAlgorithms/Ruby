df1d3b24dd2045c2d02f673b8de63d4df7db57e7
Spencer King <spencerking@users.noreply.github.com>
2017-10-01 18:00:27 -0500

    Traversals and invert

---
+++ data_structures/binary_trees/inorder_traversal.rb
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def inorder_traversal(root)
+    ans = []
+    def traverse(node, ans)
+        if node != nil
+            traverse(node.left, ans)
+            ans.push(node.val)
+            traverse(node.right,ans)
+        end
+    end
+    traverse(root,ans)
+    return ans
+end
---
+++ data_structures/binary_trees/postorder_traversal.rb
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def postorder_traversal(root)
+    ans = []
+    def traverse(node, ans)
+        if node != nil
+            traverse(node.left, ans)
+            traverse(node.right,ans)
+            ans.push(node.val)
+        end
+    end
+    traverse(root,ans)
+    return ans
+end

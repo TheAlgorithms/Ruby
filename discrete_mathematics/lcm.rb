3fcd4b28a0d1639ad78649fef9e54194428fe933
Vitor <vitor.carminatti@hotmail.com>
2019-02-18 09:20:35 -0300

    Changed folder and added simple user interface

---
+++ discrete_mathematics/lcm.rb
@@ -0,0 +1,23 @@
+# LCM (Least Common Multiple) of two numbers is the smallest number which can be divided by both numbers.
+
+p "Least Common Multiple"
+
+p "Enter first number"
+value_one = gets.chomp.to_i
+
+p "Enter second number"
+value_two = gets.chomp.to_i
+
+def gcd(first, second)
+  if second != 0
+    gcd(second, first%second)
+  else
+    first
+  end
+end
+
+def lcm(first, second)
+  (first * second)/gcd(first, second)
+end
+
+p "Least Common Multiple is: #{lcm(value_one, value_two)}"

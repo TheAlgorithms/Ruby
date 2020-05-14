9c1823739c50d233d2ac0546a7718602dacacac5
Doppon <zaxpkuraudo7777@gmail.com>
2020-05-14 11:05:14 +0900

    fix syntax of data_structures

--- data_structures/binary_trees/preorder_traversal.rb
+++ data_structures/binary_trees/preorder_traversal.rb
@@ -1,23 +1,23 @@
 # Definition for a binary tree node.
 # class TreeNode
-#     attr_accessor :val, :left, :right
-#     def initialize(val)
-#         @val = val
-#         @left, @right = nil, nil
-#     end
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
 # end
 
 # @param {TreeNode} root
 # @return {Integer[]}
 def preorder_traversal(root)
-    ans = []
-    def traverse(node, ans)
-        if node != nil
-            ans.push(node.val)
-            traverse(node.left, ans)
-            traverse(node.right,ans)
-        end
+  ans = []
+  def traverse(node, ans)
+    if node != nil
+      ans.push(node.val)
+      traverse(node.left, ans)
+      traverse(node.right, ans)
     end
-    traverse(root,ans)
-    return ans
+  end
+  traverse(root, ans)
+  return ans
 end

29f5afc96aeb32591626562c50c4511cd1a535ba
Anubhav Jain <anubhavj1357@gmail.com>
2020-10-18 15:45:59 +0530

    Add question 22 details in the solution file

--- Project Euler/Problem 22/problem22_sol1.rb
+++ Project Euler/Problem 22/problem22_sol1.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# Problem 22
+# Using names.txt (right click and 'Save Link/Target As...'),
+# a 46K text file containing over five-thousand first names,
+# begin by sorting it into alphabetical order.
+# Then working out the alphabetical value for each name,
+# multiply this value by its alphabetical position in the list to obtain a name score.
+
+# For example, when the list is sorted into alphabetical order,
+# COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+# is the 938th name in the list. So, COLIN would obtain a score of 938 * 53 = 49714.
+
+# What is the total of all the name scores in the file?
+
 # reading the contents of the file
 file_contents = File.open('p022_names.txt', 'r').read
 

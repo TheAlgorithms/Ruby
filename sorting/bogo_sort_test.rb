76d3fac587fc72c4f951af98ade3fa48031ace72
vzvu3k6k <vzvu3k6k@gmail.com>
2020-12-31 02:29:48 +0900

    Extract sort test cases to SortTests

--- sorting/bogo_sort_test.rb
+++ sorting/bogo_sort_test.rb
@@ -1,16 +1,11 @@
 require 'minitest/autorun'
+require_relative './sort_tests'
 require_relative './bogo_sort'
 
 class TestBogoSort < Minitest::Test
-  def test_sorted_array
-    input = [1, 2, 3, 4, 5]
-    expected = input.dup
-    assert_equal expected, input.bogosort
-  end
+  include SortTests
 
-  def test_reversed_array
-    input = [5, 4, 3, 2, 1]
-    expected = [1, 2, 3, 4, 5]
-    assert_equal expected, input.bogosort
+  def sort(input)
+    input.bogosort
   end
 end

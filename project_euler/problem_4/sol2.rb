770fc9622c5a42ee5ee6d32cd0915361e73d3de5
vzvu3k6k <vzvu3k6k@gmail.com>
2020-12-31 00:44:04 +0900

    Add problem description copying from sol1.rb

--- project_euler/problem_4/sol2.rb
+++ project_euler/problem_4/sol2.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 class Integer
   def parindrome?
     self == reverse

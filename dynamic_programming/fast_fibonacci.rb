b1f6170d026b0d98af78a97f719c2e61cf3c593a
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 12:15:05 -0800

    Update dynamic_programming/fast_fibonacci.rb
    Co-authored-by: vzvu3k6k <vzvu3k6k@gmail.com>

--- dynamic_programming/fast_fibonacci.rb
+++ dynamic_programming/fast_fibonacci.rb
@@ -4,5 +4,5 @@ def fast_fibonacci(number, memo_hash = {})
     if number == 0 || number == 1
         return number
     end
-    memo_hash[number] ||= fibonacci(number - 1, memo_hash) + fibonacci(number -2, memo_hash)
+    memo_hash[number] ||= fibonacci(number - 1, memo_hash) + fibonacci(number - 2, memo_hash)
 end

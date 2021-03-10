92f09581f6115c25374d798bd10ab32ea4706452
Jessica Kwok <jessicatckwok@gmail.com>
2021-03-10 14:27:54 -0800

    Add two pointers approach

--- data_structures/arrays/sort_squares_of_an_array.rb
+++ data_structures/arrays/sort_squares_of_an_array.rb
@@ -46,3 +46,41 @@ def sorted_squares(nums)
   bubble_sort(nums)
 end
 print(sorted_squares([4, -1, -9, 2]))
+
+#
+# Approach 3: solving without ruby sort method. Using two-pointers
+#
+# Time complexity: O(n)
+# Space complexity: O(n)
+#
+def sorted_squares(nums)
+  p1 = 0
+  p2 = nums.length - 1
+  # since we're returing the result in ascending order,
+  # we'll fill in the array from the end
+  max_index = p2
+  output = []
+  while p1 < p2
+    nums1_square = nums[p1] * nums[p1]
+    nums2_square = nums[p2] * nums[p2]
+    if nums1_square < nums2_square
+      output[max_index] = nums2_square
+      p2 -= 1
+    elsif nums1_square > nums2_square
+      output[max_index] = nums1_square
+      p1 += 1
+    else
+      output[max_index] = nums1_square
+      max_index -= 1
+      output[max_index] = nums2_square
+      p1 += 1
+      p2 -= 1
+    end
+    max_index -= 1
+  end
+  # to account for any remaining value left in the input array
+  output[max_index] = nums[p1] * nums[p2] if p1 == p2
+  output
+end
+
+print(sorted_squares([4, -1, -9, 2]))

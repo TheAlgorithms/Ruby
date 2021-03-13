9429ae92c1769f47c0d8465de18a576e55a68ff8
Jessica Kwok <jessicatckwok@gmail.com>
2021-03-12 18:51:41 -0800

    Add remove elements challenge instructions

---
+++ data_structures/arrays/remove_elements.rb
@@ -0,0 +1,18 @@
+# Arrays - Remove Elements
+#
+# Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+# Do not allocate extra space for another array, 
+# you must do this by modifying the input array in-place with O(1) extra memory.
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+#
+# Example
+#
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2]
+#
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3]
+
+#
+# Approach 1: Brute Force
+#

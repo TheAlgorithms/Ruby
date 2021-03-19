2171cad18e78ff4d4d8f86a71c0c43b703301142
Vitor Oliveira <vbrazo@gmail.com>
2021-03-18 17:00:54 -0700

    Mathematical: Digital Root approach

---
+++ maths/add_digits.rb
@@ -0,0 +1,41 @@
+# Challenge name: Add Digits
+#
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+#
+# Example:
+#
+# Input: 38
+# Output: 2
+# Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+#              Since 2 has only one digit, return it.
+#
+# Follow up:
+# Could you do it without any loop/recursion in O(1) runtime?
+# @param {Integer} num
+# @return {Integer}
+
+#
+# Approach: Mathematical: Digital Root
+# https://en.wikipedia.org/wiki/Digital_root
+#
+
+#
+# Complexity Analysis
+#
+# Time Complexity: O(1).
+# Space Complexity: O(1).
+
+def add_digits(num)
+  if num == 0
+    return 0
+  elsif num % 9 == 0
+    return 9
+  end
+  return num % 9
+end
+
+puts(add_digits(38))
+# # => 2
+
+puts(add_digits(284))
+# # => 5

7cb81633bc96c53281e88b1e648bd6074103958e
Vitor Oliveira <vbrazo@gmail.com>
2021-03-18 17:05:12 -0700

    Update add_digits.rb

--- maths/add_digits.rb
+++ maths/add_digits.rb
@@ -6,13 +6,7 @@
 #
 # Input: 38
 # Output: 2
-# Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
-#              Since 2 has only one digit, return it.
-#
-# Follow up:
-# Could you do it without any loop/recursion in O(1) runtime?
-# @param {Integer} num
-# @return {Integer}
+# Explanation: 3 + 8 = 11, 1 + 1 = 2.
 
 #
 # Approach: Mathematical: Digital Root

b4806f090372d4e2fadb66d3a5bb26eae031b02f
Jessica Kwok <jessicatckwok@gmail.com>
2021-03-26 09:48:52 -0700

    Add remove vowels challenge

---
+++ data_structures/arrays/remove_vowels.rb
@@ -0,0 +1,29 @@
+# Challenge name: Remove vowels from a string
+#
+# Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u'
+# from it, and return the new string.
+# Example 1:
+# Input: s = "leetcodeisacommunityforcoders"
+# Output: "ltcdscmmntyfrcdrs"
+#
+# Example 2:
+# Input: s = "aeiou"
+# Output: ""
+#
+# @param {String} s
+# @return {String}
+
+#
+# Approach 1:
+#
+# Time Complexity:
+#
+def remove_vowels(s)
+end
+
+s = 'leetcodeisacommunityforcoders'
+print(remove_vowels(s))
+# => "ltcdscmmntyfrcdrs"
+s = 'aeiou'
+print(remove_vowels(s))
+# => ""

2fe1098805de6fb91f5b567484f01d3f90ae720a
Jessica Kwok <jessicatckwok@gmail.com>
2021-03-31 16:40:23 -0700

    Add intersection challenge

---
+++ data_structures/arrays/intersection.rb
@@ -0,0 +1,23 @@
+# Challenge name: Intersection of two arrays ii
+#
+# Given two arrays, write a function to compute their intersection.
+#
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
+
+#
+# Approach 1:
+#
+# Time Complexity:
+#
+def intersect(arr1, arr2)
+end
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+intersect(nums1, nums2)
+# => [2,2]
+nums1 = [4, 9, 5]
+nums2 = [9, 4, 9, 8, 4]
+intersect(nums1, nums2)
+# => [4,9]

a93d5cc072d98981331661186ad8650405dc6bde
Vitor Oliveira <vbrazo@gmail.com>
2021-03-31 18:38:01 -0700

    Update data_structures/arrays/strings/anagram_checker.rb

--- data_structures/arrays/strings/anagram_checker.rb
+++ data_structures/arrays/strings/anagram_checker.rb
@@ -20,6 +20,7 @@
 # Complexity analysis:
 #
 # Time Complexity: O(n log n). Assume that n is the length of s, sorting costs O(n log n), and comparing two strings costs O(n). Sorting time dominates and the overall time complexity is O(n log n).
+# Space Complexity: O(1). Space depends on the sorting implementation which, usually, costs O(1) auxiliary space if heapsort is used.
 #
 def is_anagram(s, t)
   return false if s.length != t.length

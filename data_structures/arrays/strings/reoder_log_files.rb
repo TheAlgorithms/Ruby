8805684ef85818241b4d26364fed4168bf0bda97
Vitor Oliveira <vbrazo@gmail.com>
2021-04-01 10:26:30 -0700

    Update reoder_log_files.rb

--- data_structures/arrays/strings/reoder_log_files.rb
+++ data_structures/arrays/strings/reoder_log_files.rb
@@ -4,12 +4,15 @@
 #
 # Letter-logs: All words (except the identifier) consist of lowercase English letters.
 # Digit-logs: All words (except the identifier) consist of digits.
+# 
 # Reorder these logs so that:
 #
 # The letter-logs come before all digit-logs.
 # The letter-logs are sorted lexicographically by their contents.
-# If their contents are the same, then sort them lexicographically by their identifiers.
+# 
+# If their letter-logs and digit-logs are the same, then sort them by their identifiers.
 # The digit-logs maintain their relative ordering.
+# 
 # Return the final order of the logs.
 
 # Example 1:
@@ -23,7 +26,7 @@
 #
 
 #
-# Approach 1: Comparator
+# Approach: Comparator
 #
 
 #

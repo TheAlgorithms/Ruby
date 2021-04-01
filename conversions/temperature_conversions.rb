726cce387547a8ffd4d3fc16b2d793a5ca8bca93
Vitor Oliveira <vbrazo@gmail.com>
2021-03-31 18:55:38 -0700

    small clean up

--- conversions/temperature_conversions.rb
+++ conversions/temperature_conversions.rb
@@ -1,88 +1,72 @@
 # A ruby program for temperature conversions
 
-# celsius -> kelvin = value of celsius + 273.15 => K
-def celsius_to_kelvin(celsius_input)
-  begin
+module TemperatureConversion
+  # celsius -> kelvin = value of celsius + 273.15 => K
+  def self.celsius_to_kelvin(celsius_input)
     kelvin_output = (celsius_input + 273.15).round(2)
-  rescue TypeError
-    puts "Error: Please provide number only!"
-  else
     puts "#{celsius_input}°C = #{kelvin_output}K"
+  rescue
+    puts "Error: Please provide number only!"
   end
-end
 
-# kelvin -> celsius = vale of kelvin - 273.15 => °C
-def kelvin_to_celsius(kelvin_input)
-  begin
+  # kelvin -> celsius = vale of kelvin - 273.15 => °C
+  def self.kelvin_to_celsius(kelvin_input)
     celsius_output = (kelvin_input - 273.15).round(2)
-  rescue TypeError
-    puts "Error: Please provide number only!"
-  else
     puts "#{kelvin_input}K = #{celsius_output}°C"
+  rescue
+    puts "Error: Please provide number only!"
   end
-end
 
-# celsius -> fahrenheit = (value of celsius * 9 / 5) + 32 => °F
-def celsius_to_fahrenheit(celsius_input)
-  begin
+  # celsius -> fahrenheit = (value of celsius * 9 / 5) + 32 => °F
+  def self.celsius_to_fahrenheit(celsius_input)
     fahrenheit_output = ((celsius_input * 9 / 5) + 32).round(2)
-  rescue TypeError
-    puts "Error: Please provide number only!"
-  else
     puts "#{celsius_input}°C = #{fahrenheit_output}°F"
+  rescue
+    puts "Error: Please provide number only!"
   end
-end
 
-# fahrenheit -> celsius = (value of fahrenheit - 32) * 5 / 9 => °C
-def fahrenheit_to_celsius(fahrenheit_input)
-  begin
+  # fahrenheit -> celsius = (value of fahrenheit - 32) * 5 / 9 => °C
+  def self.fahrenheit_to_celsius(fahrenheit_input)
     celsius_output = ((fahrenheit_input - 32) * 5 / 9).round(2)
-  rescue TypeError
-    puts "Error: Please provide number only!"
-  else
     puts "#{fahrenheit_input}°F = #{celsius_output}°C"
+  rescue
+    puts "Error: Please provide number only!"
   end
-end
 
-# fahrenheit -> kelvin = [(value of fahrenheit - 32) * 5 / 9] + 273.15 => K
-def fahrenheit_to_kelvin(fahrenheit_input)
-  begin
+  # fahrenheit -> kelvin = [(value of fahrenheit - 32) * 5 / 9] + 273.15 => K
+  def self.fahrenheit_to_kelvin(fahrenheit_input)
     kelvin_output = ((fahrenheit_input - 32) * 5 / 9).round(2).round(2)
-  rescue TypeError
-    puts "Error: Please provide number only!"
-  else
     puts "#{fahrenheit_input}°F = #{kelvin_output}K"
-  end
-end
-
-# kelvin -> fahrenheit = [(value of kelvin - 32) * 5 / 9] + 273.15 => K
-def kelvin_to_fahrenheit(kelvin_input)
-  begin
-    fahrenheit_output = (((kelvin_input - 273.15) * 9 / 5) + 32).round(2).round(2)
-  rescue TypeError
+  rescue
     puts "Error: Please provide number only!"
-  else
+  end
+
+  # kelvin -> fahrenheit = [(value of kelvin - 32) * 5 / 9] + 273.15 => K
+  def self.kelvin_to_fahrenheit(kelvin_input)
+    fahrenheit_output = (((kelvin_input - 273.15) * 9 / 5) + 32).round(2).round(2)
     puts "#{kelvin_input}K = #{fahrenheit_output}°F"
+  rescue
+    puts "Error: Please provide number only!"
   end
 end
 
 # celsius <-> kelvin
-celsius_to_kelvin(20)
-kelvin_to_celsius(20)
+TemperatureConversion.celsius_to_kelvin(20)
+TemperatureConversion.kelvin_to_celsius(20)
 
 # Invalid input
-kelvin_to_celsius("a")
+TemperatureConversion.kelvin_to_celsius("a")
 
 # celsius <-> fahrenheit
-celsius_to_fahrenheit(-20)
-fahrenheit_to_celsius(68)
+TemperatureConversion.celsius_to_fahrenheit(-20)
+TemperatureConversion.fahrenheit_to_celsius(68)
 
 # Invalid input
-celsius_to_fahrenheit("abc")
+TemperatureConversion.celsius_to_fahrenheit("abc")
 
 # fahrenheit <-> kelvin
-fahrenheit_to_kelvin(60)
-kelvin_to_fahrenheit(-60)
+TemperatureConversion.fahrenheit_to_kelvin(60)
+TemperatureConversion.kelvin_to_fahrenheit(-60)
 
 # Invalid input
-fahrenheit_to_kelvin("60")
+TemperatureConversion.fahrenheit_to_kelvin("60")

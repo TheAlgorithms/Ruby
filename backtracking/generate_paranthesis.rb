1d974b2ea18e646fe277ada716902d42ed95c659
Vitor Oliveira <vbrazo@gmail.com>
2021-04-01 09:58:04 -0700

    Add approach description

--- backtracking/generate_paranthesis.rb
+++ backtracking/generate_paranthesis.rb
@@ -15,6 +15,16 @@
 #
 # 1 <= n <= 8
 
+# Approach:
+#
+# Let's only add '(' or ')' when we know it will remain a valid sequence.
+# We can do this by keeping track of the number of opening and closing brackets
+# we have placed so far.
+#
+# We can start an opening bracket if we still have one (of n) left to place.
+# And we could start a closing bracket if it'd not exceed the number of opening
+# brackets.
+
 # @param {Integer} n
 # @return {String[]}
 def generate_parenthesis(n)

7b20bca2f82d02d9bc7046bc7d386a11f7389c84
Jessica Kwok <jessicatckwok@gmail.com>
2021-04-16 16:21:49 -0700

    Add hash solution

--- data_structures/arrays/intersection.rb
+++ data_structures/arrays/intersection.rb
@@ -42,3 +42,35 @@ nums1 = [4, 9, 5]
 nums2 = [9, 4, 9, 8, 4]
 puts intersect(nums1, nums2)
 # => [4,9]
+
+#
+# Approach 2: Hash
+#
+# Time Complexity: O(n)
+#
+def intersect(arr1, arr2)
+  result = []
+
+  hash = Hash.new(0)
+
+  arr2.each {|num| hash[num] += 1 }
+
+  arr1.each do |num|
+    if hash.has_key?(num)
+      result << num if hash[num] >= 1
+      hash[num] -= 1
+    end
+  end
+  
+  result
+end
+
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+puts intersect(nums1, nums2)
+# => [2,2]
+
+nums1 = [4, 9, 5]
+nums2 = [9, 4, 9, 8, 4]
+puts intersect(nums1, nums2)
+# => [4,9]

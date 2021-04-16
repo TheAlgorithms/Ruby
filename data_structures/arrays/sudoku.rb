211bb2c23f18b1e84b2f365ef455d986987f65f0
Jessica Kwok <jessicatckwok@gmail.com>
2021-04-16 16:57:39 -0700

    Added sudoku challenge

---
+++ data_structures/arrays/sudoku.rb
@@ -0,0 +1,40 @@
+# Challenge name: Valid Sudoku
+
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+# @param {Character[][]} board
+# @return {Boolean}
+
+#
+# Approach 1:
+#
+# Time Complexity:
+#
+def is_valid_sudoku(board)
+end
+
+board = [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+print(is_valid_sudoku(board))
+# => true
+board = [["8","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+print(is_valid_sudoku(board))
+# => false

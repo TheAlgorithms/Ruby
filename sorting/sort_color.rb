42dafdfe7aec3ada31cb7b2d1aeee562b04c2cd1
Vitor Oliveira <vbrazo@gmail.com>
2021-04-24 23:21:08 -0700

    enter

--- sorting/sort_color.rb
+++ sorting/sort_color.rb
@@ -34,12 +34,14 @@ def bubble_sort(array)
 
   loop do
     swapped = false
+
     (array_length - 1).times do |i|
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
     end
+
     break unless swapped
   end
 

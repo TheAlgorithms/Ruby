d7f266aa8694a2069b8fe78a60b7c0b9776d3ebd
Vijay Siva <vijay.sivasubramaniam@instacart.com>
2021-05-01 17:38:17 -0400

    Add bit_manipulation algorithms

---
+++ bit_manipulation/binary_and_operator.rb
@@ -0,0 +1,22 @@
+def binary_and(x, y)
+  raise 'Input must only contain positive integers' if x < 0 or x < 0
+
+  "0b" + (x & y).to_s(2)
+end
+
+begin
+  binary_and(-1, 0)
+rescue => e
+  puts e.message
+end
+
+puts binary_and(1, 1)
+# 0b1
+puts binary_and(0, 1)
+# 0b0
+puts binary_and(1024, 1024)
+# 0b10000000000
+puts binary_and(0, 1023)
+# 0b0000000000
+puts binary_and(16, 58)
+# 0b010000

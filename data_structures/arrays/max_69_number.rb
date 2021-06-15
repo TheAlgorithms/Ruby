1ef5a9ab07b7aa8b96a35b2b1e67010ac07c7251
Jessica Kwok <jessicatckwok@gmail.com>
2021-06-15 16:14:06 -0700

    Add max 69 number challenge

---
+++ data_structures/arrays/max_69_number.rb
@@ -0,0 +1,19 @@
+# Challenge name: Maximum 69 number
+#
+# Given a positive integer num consisting only of digits 6 and 9. 
+# Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+#
+# Example 1:
+# Input: num = 9669
+# Output: 9969
+# Explanation:
+# Changing the first digit results in 6669.
+# Changing the second digit results in 9969.
+# Changing the third digit results in 9699.
+# Changing the fourth digit results in 9666.
+# The maximum number is 9969.
+#
+# Example 2:
+# Input: num = 9996
+# Output: 9999
+# Explanation: Changing the last digit 6 to 9 results in the maximum number.

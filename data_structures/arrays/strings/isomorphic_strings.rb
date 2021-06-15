2e354335973a84593703f4e6aea96a9c36953e49
Jessica Kwok <jessicatckwok@gmail.com>
2021-06-15 16:09:38 -0700

    Add isomorphic strings challenge

---
+++ data_structures/arrays/strings/isomorphic_strings.rb
@@ -0,0 +1,23 @@
+# Challenge name: Isomorphic Strings
+#
+# Given two strings s and t, determine if they are isomorphic.
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+# All occurrences of a character must be replaced with another character while preserving the order of characters. 
+# No two characters may map to the same character, but a character may map to itself.
+#
+# Example 1:
+# Input: s = "egg", t = "add"
+# Output: true
+#
+# Example 2:
+# Input: s = "foo", t = "bar"
+# Output: false
+#
+# Example 3:
+# Input: s = "paper", t = "title"
+# Output: true
+#
+# Constraints:
+# 1 <= s.length <= 5 * 104
+# t.length == s.length
+# s and t consist of any valid ascii character.

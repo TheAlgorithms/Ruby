e804e6814c1ee91743d9df6e6250bc387fb56fe6
Jessica Kwok <jessicatckwok@gmail.com>
2021-07-08 14:57:00 -0700

    Add Date.parse solution

--- other/number_of_days.rb
+++ other/number_of_days.rb
@@ -12,3 +12,23 @@
 # Output: 15
 #
 # Constraints: The given dates are valid dates between the years 1971 and 2100.
+
+
+#
+# Approach 1: Using Date.parse
+# Time complexity: O(1)
+# Space complexity: O(1)
+#
+require 'date'
+
+def number_of_days(date1, date2)
+  beginning_date = Date.parse(date1)
+  end_date = Date.parse(date2)
+  (end_date - beginning_date).to_i.abs
+end
+
+puts number_of_days("2019-06-29", "2019-06-30")
+# => 1
+
+puts number_of_days("2020-01-15", "2019-12-31")
+# => 15

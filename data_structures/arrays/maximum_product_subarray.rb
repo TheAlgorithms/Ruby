f6382bfb15b9430af9f874d834bf222cbca5d322
Vitor Oliveira <vbrazo@gmail.com>
2021-09-03 12:37:45 -0700

    kill space

--- data_structures/arrays/maximum_product_subarray.rb
+++ data_structures/arrays/maximum_product_subarray.rb
@@ -1,4 +1,3 @@
-
 # Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
 # It is guaranteed that the answer will fit in a 32-bit integer.
 # A subarray is a contiguous subsequence of the array.

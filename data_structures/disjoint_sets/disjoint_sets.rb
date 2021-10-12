87db7cf633bd3d155370d5731db452e9fbaf53c1
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-10-12 10:21:38 +0300

    fixed importing issue

--- data_structures/disjoint_sets/disjoint_sets.rb
+++ data_structures/disjoint_sets/disjoint_sets.rb
@@ -1,4 +1,23 @@
-require "./data_structures/disjoint_sets/node.rb"
+class Node
+    attr_accessor :data, :parent, :rank
+    def initialize(data)
+        @data = data
+        @parent = self
+        @rank = 0
+    end
+    def parent
+        @parent
+    end
+    def parent=(parent)
+        @parent = parent;
+    end
+    def rank
+        @rank
+    end
+    def rank=(rank)
+        @rank = rank
+    end
+end
 
 class DisjointSets
     def make_set(d)

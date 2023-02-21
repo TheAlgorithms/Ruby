bb5df875377e3c9d4dfb918e8b554cde9b6b3a19
domix80 <d.agresta@gmail.com>
2023-02-21 18:36:46 +0100

    Update project_euler/problem_006/sol1.rb
    Co-authored-by: Amos Paribocci <aparibocci@gmail.com>

--- project_euler/problem_006/sol1.rb
+++ project_euler/problem_006/sol1.rb
@@ -11,8 +11,7 @@
 #Find the difference between the sum of the squares of the first one
 #hundred natural numbers and the square of the sum.
 
-def solution?()
-	num = 10
+def solution(num=10)
 	x = 1
 	y = 1
 	result = 1
@@ -29,5 +28,5 @@ def solution?()
 	r_sum_n_pow2 - r_n_pow2_plus_n_pow2
 end
 
-answer = solution?()
+answer = solution()
 p answer

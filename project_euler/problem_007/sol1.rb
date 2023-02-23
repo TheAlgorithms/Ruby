4420ff4172a87348054b5bbd462a2d3f6c2616f7
domix80 <d.agresta@gmail.com>
2023-02-23 16:02:35 +0100

    Update project_euler/problem_007/sol1.rb
    Co-authored-by: Stepfen Shawn <m18824909883@163.com>

--- project_euler/problem_007/sol1.rb
+++ project_euler/problem_007/sol1.rb
@@ -1,4 +1,4 @@
-#Project Euler Problem 7: #https://projecteuler.net/problem=7
+#Project Euler Problem 7:  https://projecteuler.net/problem=7
 #10001st prime
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, #and 13, we
 #can see that the 6th prime is 13.
